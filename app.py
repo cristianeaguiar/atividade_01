76f6fff7ac86e4d527566c35aabfbf3d1c06b368
cristianeaguiar <cristiane.aguiarsa@gmail.com>
2024-09-23 21:14:26 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,22 @@
+# entrada de dados
+nome = input("Informe seu nome: ")
+cpf = int(input ("Informe seu CPF: "))
+telefone = int(input ("Informe seu telefone: "))
+email = input("Informe seu e-mail: ")
+endereco = input("Informe seu Endereço: ")
+genero = input("Informe seu Gênero: ")
+escolaridade = input("Informe sua Escolaridade: ")
+signo = input("Informe seu Signo: ")
+tipo_sanguineo = input("Informe sua Tipo Sanguíneo: ")
+
+# saída de dados
+
+print(nome) 
+print(cpf)
+print(telefone)
+print(email)
+print(endereco)
+print(genero)
+print(escolaridade)
+print(signo)
+print(tipo_sanguineo)
